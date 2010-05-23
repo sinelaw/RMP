a3b45693ef386d420196099ca1c192b5196964da
Noam <noam@duapov.(none)>
2010-05-23 21:22:17 +0300

    Tuned the face test

--- src/FaceFollowTest.hs
+++ src/FaceFollowTest.hs
@@ -11,8 +11,8 @@ import AI.CV.OpenCV.Types(PImage)
 
 import System.RMP(velocityRMP)
 
-import Control.Processor(runUntil, IOProcessor, (--<), trace, fir, revertAfterT, holdMaybe)
-import Data.VectorSpace(zeroV, (^-^), (^*), AdditiveGroup)
+import Control.Processor(IOProcessor, trace, fir, revertAfterT, holdMaybe)
+import Data.VectorSpace(zeroV, (^-^), AdditiveGroup)
 
 import Control.Monad(join)
 import Prelude hiding ((.),id)
@@ -20,8 +20,13 @@ import Control.Arrow
 import Control.Category
 import Data.Maybe(listToMaybe)
 
+-- Debugging
 import qualified Debug.Trace as DT
-traceId x = DT.trace (show x) x
+traceId :: (Show a) => a -> a
+--traceId x = DT.trace (show x) x
+traceId = id
+--
+
 
 defaultHead :: a -> [a] -> a
 defaultHead def [] = def
@@ -40,14 +45,22 @@ fromIntegral2 :: (Integral b, Num c) => (b, b) -> (c, c)
 fromIntegral2 = join (***) fromIntegral
 
 
+absMax :: (Num a, Ord a) => a -> a -> a
 absMax b a = max (min a (abs b)) (- (abs b))
 -----------------------------------------------------------------------------
 
+-- | Calculates a measure for the distance to a rect using its area, given a reference area size.
 calcDist :: (Num x, Ord x) => x -> CvRect -> x
 calcDist reference rect = if rectArea > 1 then reference - rectArea else 0
     where w = rectWidth  rect
           h = rectHeight rect
           rectArea = traceId . uncurry (*) $ fromIntegral2 (w,h)
+
+-- | Calculates a distance to the given rect using some hand-tuned parameters 
+calcTrans :: (Integral b, Integral a) => a -> a -> CvRect -> b
+calcTrans resX resY = (`div` tranScale) . traceId . calcDist referenceArea
+    where referenceArea = fromIntegral ((resX*resY) `div` 30)
+          tranScale = 20 -- 5 for 160x120?
 
 -- | Calculates the difference (direction) from the detect rect to the center of the screen.
 -- the 'fromIntegral2' stuff is due to CInt not being a VectorSpace
@@ -59,14 +72,15 @@ calcDir resX resY rect = if rect /= zeroV then rectCenter ^-^ screenCenter else 
   
 -- | Takes a direction vector (x,y) and returns required rotation speed to align with that direction.
 -- for now we disregard the 'x' component, because we can't really point our robot "up" or "down" anyway.
-dirToRotation :: (Num a, Ord a, Num b, Ord b) => (a,b) -> a
-dirToRotation (yRot, xRot) = - (absMax 200 (yRot*4) )
+dirToRotation :: (Num a, Ord a, Integral a, Num b, Ord b) => (a,b) -> a
+dirToRotation (yRot, _) = - round (fromIntegral yRot * rotScale)
+    where rotScale = 1.4 -- for 160x120, should be 4?
   
 -- | calculates the (translation, rotation) pair used to control the robot, from a detected rect.
 -- currently translation is constantly 0.
 calcTransRot :: (Num c, Ord c, Integral c, Integral a, Integral b, AdditiveGroup b) => a -> a -> CvRect -> (c, b)
-calcTransRot resX resY = (absMax 40 . (flip div 5) . calcDist (fromIntegral ((resX*resY) `div` 30)))  &&& 
-                         (calcDir resX resY >>> dirToRotation)
+calcTransRot resX resY = (calcTrans resX resY >>> absMax maxTransVelocity)  
+                         &&& (calcDir resX resY >>> dirToRotation >>> absMax maxRotVelocity)
 
 -- todo: a better solution than choosing the default if no faces detected, would be to keep tracking the last
 -- known face?
@@ -76,14 +90,20 @@ controller resX resY =  arr (calcTransRot resX resY)
 clock :: IO Double
 clock = return 1 -- todo implement really in some module that wraps SDL, GLUT or whatever.
 
+-- | The maximum rotational and translational velocity of the robot
+maxRotVelocity, maxTransVelocity :: Integral a => a
+maxTransVelocity = 40
+maxRotVelocity = 150
+
 main :: IO ()  
 main = runTillKeyPressed (videoSource >>> imageResizeTo resX resY 
-             >>> (id &&& averageFace) >>> ((second (faceToVel >>> trace >>> velocityRMP)) &&& showVideo))
+             >>> (id &&& averageFace) >>> second (faceToVel >>> trace >>> velocityRMP) &&& showVideo)
       where showVideo = (second . arr $ return) >>> ImageProcessors.drawRects >>> ImageProcessors.window 0
-            averageFace = fir [0.9,0.1] 1 clock lastFace
+            --showVideo = arr (const ()) -- does nothing
+            averageFace = lastFace --fir [0.9,0.1] 1 clock lastFace
             lastFace = revertAfterT 5 zeroV . holdMaybe zeroV clock $ (faceDetect >>> arr listToMaybe)
-            resX = 160
-            resY = 120
+            resX = 240
+            resY = 180
             faceToVel = controller resX resY
 
 
